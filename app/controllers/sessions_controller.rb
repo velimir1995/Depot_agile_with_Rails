5536d5cb9a2b6226139e5531c5ba5b059ea25e03
Velimir Gvozdenac <vgvozdenac.vg@gmail.com>
2022-03-22 13:08:48 +0100

    removing last changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:name])
-    if User.all.size == 0
-      session[:user_id] = "very_first"
-    elsif user.try(:authenticate, params[:password])
+    if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
